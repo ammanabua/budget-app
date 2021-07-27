dba4acdbff41def616f4c4a857a133e7f85e20ac
Amman Abua <ammanabua@gmail.com>
2021-07-27 19:49:19 +0100

    add delete functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <AppProvider>
 
         <div className='container'>
-              <h1 className='mt-3'>Budget Planner</h1>  
+              <h1 className='mt-3'>Budget Tracker</h1>  
               <div className='row mt-3'>
                 <div className='col-sm'>
                   <Budget /> 
